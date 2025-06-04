2b2f5d82e542e7d7796478bd1a336d660ea0562d
Paul Vinueza <paulvinueza30@gmail.com>
2025-06-04 02:31:26 -0400

    ui changes

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-from routers.translation_router import translation_router
+from fastapi.middleware.cors import CORSMiddleware
+from routes.routes import setup_routes
 import uvicorn
 
 app = FastAPI()
-app.include_router(translation_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+setup_routes(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3000)
